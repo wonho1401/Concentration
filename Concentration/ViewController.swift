210777d05fa92d8059cdcb6f627c52f249d71c7c
wonho <wonho1401@naver.com>
2021-10-05 11:49:09 +0900

    Lesson1

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Concentration
+//  Concentration/Users/jeong-wonho/Documents/swiftEx/Concentration/Concentration/Base.lproj/Main.storyboard
 //
 //  Created by 정원호 on 2021/09/29.
 //
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var flipCount: Int = 0 {
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
+    
+    var emojiChoices = ["🎃","👻","🎃","👻"]
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender){
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else{
+            print("Chosen card was not in cardButtons.")
+        }
+        
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji{
+            button.setTitle("", for: .normal)
+            button.backgroundColor = .orange
+        } else{
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = .white
+        }
+    }
 
 }
 
