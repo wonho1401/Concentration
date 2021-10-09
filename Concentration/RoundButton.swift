cefb8d78495131f56b10b757c85cdb46b44f28be
wonho <wonho1401@naver.com>
2021-10-09 17:56:37 +0900

    style, line break, corner radius

---
+++ Concentration/RoundButton.swift
@@ -0,0 +1,30 @@
+//
+//  roundButton.swift
+//  Concentration
+//
+//  Created by 정원호 on 2021/10/09.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
